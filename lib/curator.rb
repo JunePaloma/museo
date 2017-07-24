4a8b3e3fe488d136b76f952d807389ca3f360bb7
JunePaloma <kborkoski@gmail.com>
2017-07-24 10:09:12 -0600

    Add all the things I think

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,2 +1,24 @@
 class Curator
+  attr_reader :artists,
+              :museums,
+              :photographs
+
+  def initialize
+    @artists = []
+    @museums = []
+    @photographs = []
+  end
+
+  def add_museum(musume)
+    @museums << (musume)
+  end
+
+  # def museum_count
+  #   @museums.length
+  # end
+
+  def add_artist(artist)
+    @artists << (artist)
+  end
+
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -4,4 +4,43 @@ require 'minitest/pride'
 require './lib/curator'
 
 class CuratorTest < Minitest::Test
+
+  def setup
+   @curator = Curator.new
+  end
+
+  def test_curator_has_artist_arr
+    assert_equal [], @curator.artists
+  end
+
+  def test_curator_has_museums
+    assert_equal [], @curator.museums
+  end
+
+  def test_curator_has_photographs
+    assert_equal [], @curator.photographs
+  end
+
+  def test_museums_can_be_added
+    @curator.add_museum({name: "MoMA"})
+    assert_equal 1, @curator.museums.count
+  end
+
+  def test_museum_id
+    @curator.add_museum({name: "MoMA"})
+  assert_equal 1, @curator.museums.first.id
+  end
+
+  def test_artist_can_be_added
+    @curator.add_artist({name: "Ansel Adams", born: 1902, died: 1984, country: "United States"})
+    assert_equal 1, @curator.artists.count
+  end
+
+  def test_artist_has_id
+    @curator.add_artist({name: "Ansel Adams", born: 1902, died: 1984, country: "United States"})
+    assert_equal 1, @curator.arists.first.id
+
+  end
+
+
 end
